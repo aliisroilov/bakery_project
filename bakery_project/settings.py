3fe303c978b993880376e88ea15ef058c0e15efb
Ali <aliisroilov4122@mail.ru>
2025-10-04 17:25:05 +0500

    Switch database config for PostgreSQL on Render

--- bakery_project/settings.py
+++ bakery_project/settings.py
@@ -90,7 +90,9 @@ TEMPLATES = [
 # =========================
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
