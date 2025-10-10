b38ed47f4b264713e517fc54daa336a86e9c5239
Ali <aliisroilov4122@mail.ru>
2025-10-10 15:34:00 +0500

    Update

--- bakery_project/settings.py
+++ bakery_project/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 # Static & Media
 # =========================
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
