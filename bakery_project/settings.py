45dfbf439a989e9f3b28be9c6c1374fd777cd7c9
Ali <aliisroilov4122@mail.ru>
2025-10-10 16:30:59 +0500

    Update settings for PostgreSQL

--- bakery_project/settings.py
+++ bakery_project/settings.py
@@ -131,15 +131,12 @@ USE_TZ = True
 # Static & Media
 # =========================
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # =========================
 # Default Primary Key
